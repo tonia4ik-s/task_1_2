1dbcb159facc6fa5da6dbd40d128e8da53941e4d
Task_1_1 <cooltone4ka@gmail.com>
2018-10-17 21:48:24 +0300

    origin

---
+++ src/main/java/com/tonia/Main.java
@@ -0,0 +1,32 @@
+package com.tonia;
+
+public class Main {
+    public static void main(String[]args){
+        double number = 4.2;
+        int price=20;
+        System.out.println((int)number+price);
+        // играемся с текстом
+        String adress ="Cegliana street";
+        String house = "24/7";
+        System.out.println(adress + " " + house);
+        // char это...
+        char h = 12;
+        char l = 39;
+        System.out.println(h);
+        System.out.println((int)l);
+        // float): математически округляет, ставит точку после первой цыфры,если ее нет
+        float something = 234786.57643F;
+        float numberx = 12.3425786F;
+        float x = 145673024865568976549F;
+        System.out.println(x);
+        System.out.println(numberx);
+        System.out.println(something);
+
+
+
+
+
+
+
+    }
+}
